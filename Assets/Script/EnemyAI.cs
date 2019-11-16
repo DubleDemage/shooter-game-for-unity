30ea5d60a7937a0118b76b253487fc53675d2d56
Ahmad <cassdy-91@hotmail.com>
2019-11-16 13:18:10 +0000

    Enemy AI
    Enemy AI attacks when provoked by the player

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,7 +7,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public Transform player;
+    public float chaseRange = 10f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToPlayer = Mathf.Infinity;
+
+    bool isProvoked = false;
 
 
     // Start is called before the first frame update
@@ -18,6 +24,43 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceToPlayer = Vector3.Distance(player.position, transform.position);
+        if (isProvoked)
+        {
+            AttackTarget();
+        }
+
+        else if(distanceToPlayer < chaseRange)
+        {
+            isProvoked = true;
+        }
+        
+    }
+
+    private void AttackTarget()
+    {
+        if(distanceToPlayer >= navMeshAgent.stoppingDistance)
+        {
+            ChasePlayer();
+        }
+        if(distanceToPlayer <= navMeshAgent.stoppingDistance)
+        {
+            ShootTarget();
+        }
+    }
+
+    private void ShootTarget()
+    {
+        Debug.Log(name + " hit " + player.name);
+    }
+
+    private void ChasePlayer()
+    {
         navMeshAgent.SetDestination(player.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
