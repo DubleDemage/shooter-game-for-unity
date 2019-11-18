14568eab03f15023e611c80ddc474093e529e2c1
Ahmad <cassdy-91@hotmail.com>
2019-11-18 20:22:30 +0000

    Player Shoot + Enemy Health
    Added Player Gun
    Added Enemy Health
    Added Player can kill enemy
    Added effects to the gun

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Camera FPCamera;
+    public float range = 100f;
+    public float damage = 30f;
+    public ParticleSystem muzzleFlash;
+
+ 
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PlayMuzzleFlash();
+            Shoot();
+        }
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void Shoot()
+    {
+        ProcessRaycast();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit this thing: " + hit.transform.name);
+            //add hit effects to the weapon
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
