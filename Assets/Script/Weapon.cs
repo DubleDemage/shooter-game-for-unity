e3b8265b39ed08757c532971dcdbb65f751fefd9
Ahmad <cassdy-91@hotmail.com>
2019-11-19 11:58:03 +0000

    Player Weapon
    Gave the player a weapon and made him shoot using Raycast

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Camera FPCamera;
+    public float range = 100f;
+
+    public ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            MuzzleFlashEffect();
+        }
+    }
+
+    private void MuzzleFlashEffect()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit this thing: " + hit.transform.name);
+        } 
+        else
+        {
+            return;
+        }
+    }
+}
