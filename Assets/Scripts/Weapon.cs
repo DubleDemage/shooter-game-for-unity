ecc24f01735f50568203c9ec0dfa0fe372a499d2
Ahmad <cassdy-91@hotmail.com>
2019-11-21 13:35:18 +0000

    Enemy Death + Sparkle effect
    Enemy dies from our shots
    It produces sparkle effect upon impact with any object

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     public float range = 100f;
     public float damage = 30f;
     public ParticleSystem muzzleFlash;
+    public GameObject hitEffect;
 
  
     void Update()
@@ -35,8 +36,7 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit this thing: " + hit.transform.name);
-            //add hit effects to the weapon
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
             target.TakeDamage(damage);
@@ -45,5 +45,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact =  Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
     }
 }
