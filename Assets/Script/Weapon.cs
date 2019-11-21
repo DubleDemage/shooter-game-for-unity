3ebfec8bc9f6307cf0d77ab1e42539cf4d528811
Ahmad <cassdy-91@hotmail.com>
2019-11-21 14:11:43 +0000

    Enemy Health + Sparkle effect
    Added enemy health
    And added sparkle effect for when weapon is fired and the player can see the effect

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -8,8 +8,10 @@ public class Weapon : MonoBehaviour
 
     public Camera FPCamera;
     public float range = 100f;
+    public float damage = 20f;
 
     public ParticleSystem muzzleFlash;
+    public GameObject hitEffect;
 
     // Update is called once per frame
     void Update()
@@ -31,11 +33,20 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit this thing: " + hit.transform.name);
+            CreateHitImpact(hit);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
         } 
         else
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
     }
 }
