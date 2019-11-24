6b4c857b999c3d285cb3f458e3c5141c3593e73d
Ahmad <cassdy-91@hotmail.com>
2019-11-24 14:59:53 +0000

    bulshit
    bullshit

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    PlayerHealth player;
+    public float damage = 10f;
+    public ParticleSystem gunFlash;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (player == null) return;
+        gunFlash.Play();
+        player.TakeDamage(damage);
+        Debug.Log("bang bang");
+    }
+
+}
