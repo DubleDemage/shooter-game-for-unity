0b92b815c04d7f001d15f6f97296311ba2176770
Ahmad <cassdy-91@hotmail.com>
2019-11-24 14:24:43 +0000

    nothing
    nothing

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -51,11 +51,13 @@ public class EnemyAI : MonoBehaviour
 
     private void ShootTarget()
     {
-        Debug.Log(name + " hit " + target.name);
+        GetComponent<Animator>().SetBool("attack", true);
     }
 
     private void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("attack", false);
+        GetComponent<Animator>().SetTrigger("move");
         navMeshAgent.SetDestination(target.position);
     }
 
