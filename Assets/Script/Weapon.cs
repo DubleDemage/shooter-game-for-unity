d3cb06b0590627a34325acb08f760bd332df1f2b
Ahmad <cassdy-91@hotmail.com>
2019-12-04 12:28:28 +0000

    Weapon Switcher + Ammo
    Player can switch weapons using numbers and the scroll wheel
    Added specfic ammo to its respected weapon

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -18,6 +18,8 @@ public class Weapon : MonoBehaviour
 
     public Ammo ammoSlot;
 
+    public AmmoType ammoType;
+
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -28,14 +30,14 @@ public class Weapon : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            if(ammoSlot.GetCurrentAmmo() > 0)
+            if (ammoSlot.GetCurrentAmmo(ammoType) > 0)
             {
                 Shoot();
                 MuzzleFlashEffect();
                 audioSource.Play();
-                ammoSlot.ReduceCurrentAmmo();
+                ammoSlot.ReduceCurrentAmmo(ammoType);
             }
-            
+
         }
     }
 
--- Assets/Script/WeaponSwitcher.cs
+++ Assets/Script/WeaponSwitcher.cs
@@ -7,10 +7,64 @@ public class WeaponSwitcher : MonoBehaviour
 {
     public int currentWeapon = 0;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         SetWeaponActive();
+    }
+
+
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        //Pistol
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        //AR
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
     }
 
     private void SetWeaponActive()
@@ -32,9 +86,5 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
