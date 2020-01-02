6bd526f21e0291e9be3aeaa7d61f858116d25b5d
Ahmad <cassdy-91@hotmail.com>
2020-01-02 13:33:34 +0000

    New enemy
    added a new enemy

--- Assets/Script/EnemyAttack.cs
+++ Assets/Script/EnemyAttack.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     PlayerHealth player;
-    public float damage = 10f;
-    public ParticleSystem gunFlash;
+    public float damage;
+    //public ParticleSystem gunFlash;
 
-    public AudioClip arSound;
-    AudioSource audioSource;
+    //public AudioClip arSound;
+    //AudioSource audioSource;
 
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerHealth>();
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 
     public void AttackHitEvent()
     {
         if (player == null) return;
-        gunFlash.Play();
-        audioSource.Play();
+        //gunFlash.Play();
+        //audioSource.Play();
         player.TakeDamage(damage);
         Debug.Log("bang bang");
     }
