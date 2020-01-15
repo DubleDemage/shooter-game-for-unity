f778fc855577ce3a2bc0829c18f53d3e8af4afe0
Ahmad <cassdy-91@hotmail.com>
2020-01-15 14:02:37 +0000

    loot drop
    added loot drop to the game

--- Assets/Script/EnemyHealth.cs
+++ Assets/Script/EnemyHealth.cs
@@ -11,9 +11,16 @@ public class EnemyHealth : MonoBehaviour
 
     bool isDead = false;
 
+    public GameObject lootDrop;
+
     public bool IsDead()
     {
         return isDead;
+    }
+
+    private void Start()
+    {
+        lootDrop.SetActive(false);
     }
 
     public void Update()
@@ -39,6 +46,7 @@ public class EnemyHealth : MonoBehaviour
         }
         isDead = true;
         GetComponent<Animator>().SetTrigger("die");
+        lootDrop.SetActive(true);
     }
        
 }
